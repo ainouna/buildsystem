17ac3ece45dd6837ee784dac5ba4fe409a923c04
audioniek <ndevries@xs4all.nl>
2017-12-03 20:03:02 +0100

    libjpeg-turbo: bump version to 1.5.2

--- make/contrib-libs.mk
+++ make/contrib-libs.mk
@@ -812,7 +812,7 @@ $(D)/jpeg: $(D)/bootstrap $(ARCHIVE)/$(JPEG_SOURCE)
 #
 # jpeg_turbo
 #
-JPEG_TURBO_VER = 1.5.1
+JPEG_TURBO_VER = 1.5.2
 JPEG_TURBO_SOURCE = libjpeg-turbo-$(JPEG_TURBO_VER).tar.gz
 
 $(ARCHIVE)/$(JPEG_TURBO_SOURCE):
