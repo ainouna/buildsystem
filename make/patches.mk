6f168cdc0fee854ca8c7eeeb6436adb896b4fbbf
Audioniek <audioniek@xs4all.nl>
2017-12-06 19:15:16 +0100

    update patches.mk

--- make/patches.mk
+++ make/patches.mk
@@ -1,32 +1,26 @@
 #
-# diff helper
+# patch helper
 #
 enigma%-patch \
 neutrino%-patch \
 libstb-hal%-patch:
-	cd $(SOURCE_DIR) && diff -Nur --exclude-from=$(SCRIPTS_DIR)/diff-exclude $(subst -patch,,$@).org $(subst -patch,,$@) > $(BASE_DIR)/$(subst -patch,.patch,$@) ; [ $$? -eq 1 ]
+	( cd $(SOURCE_DIR) && diff -Nur --exclude-from=$(SCRIPTS_DIR)/diff-exclude $(subst -patch,,$@).org $(subst -patch,,$@) > $(BASE_DIR)/$(subst -patch,.patch,$@) ; [ $$? -eq 1 ] )
 
 # keeping all patches together in one file
 # uncomment if needed
 #
 
-# LIB-STB-Hal for DDT
-NEUTRINO_MP_LIBSTB_DDT_PATCHES += $(PATCHES)/libstb-hal-ddt.patch
-
 # Neutrino MP DDT
 NEUTRINO_MP_DDT_PATCHES += $(PATCHES)/neutrino-mp-ddt.patch
-
-# LIB-STB-Hal NI
-NEUTRINO_MP_LIBSTB_NI_PATCHES +=  $(PATCHES)/libstb-hal-ni.patch
+NEUTRINO_MP_LIBSTB_DDT_PATCHES += $(PATCHES)/libstb-hal-ddt.patch
 
 # Neutrino MP NI
 NEUTRINO_MP_NI_PATCHES += $(PATCHES)/neutrino-mp-ni.patch
+NEUTRINO_MP_LIBSTB_NI_PATCHES +=  $(PATCHES)/libstb-hal-ni.patch
 
-# LIB-STB-Hal for MP Tango's
-NEUTRINO_MP_LIBSTB_TANGOS_PATCHES += $(PATCHES)/libstb-hal-tangos.patch
-
-# Neutrino MP Tango's
+# Neutrino MP Tango
 NEUTRINO_MP_TANGOS_PATCHES += $(PATCHES)/neutrino-mp-tangos.patch
+NEUTRINO_MP_LIBSTB_TANGOS_PATCHES += $(PATCHES)/libstb-hal-tangos.patch
 
 # Neutrino HD2
 NEUTRINO_HD2_PATCHES += $(PATCHES)/nhd2-exp.patch
