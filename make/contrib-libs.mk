da4227befcd93de62f08d7bf1e55b46a861e25a2
Audioniek <audioniek@xs4all.nl>
2018-01-13 20:53:00 +0100

    ffmpeg arm: add ffmpeg-3.3-chunked_transfer_fix_eof.patch

--- make/contrib-libs.mk
+++ make/contrib-libs.mk
@@ -1693,7 +1693,7 @@ FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-fix-mpegts.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-allow-to-choose-rtmp-impl-at-runtime.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-add-dash-demux.patch
 FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-hls-replace-key-uri.patch
-
+FFMPEG_PATCH += ffmpeg-$(FFMPEG_VER)-chunked_transfer_fix_eof.patch
 FFMPEG_DEPS = $(D)/libxml2 $(D)/librtmpdump
 FFMPEG_CONF_OPTS  = --enable-librtmp
 FFMPRG_EXTRA_CFLAGS = -I$(TARGET_DIR)/usr/include/libxml2 -mfpu=neon-vfpv4 -mfloat-abi=hard
