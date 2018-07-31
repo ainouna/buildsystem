cf7374e906e525962d89c9c9c493f165c589fb02
Audioniek <audioniek@xs4all.nl>
2018-07-31 13:13:27 +0200

    crosstool-ng: bump version to 872341e3

--- make/crosstool-arm.mk
+++ make/crosstool-arm.mk
@@ -6,7 +6,7 @@ crosstool-renew:
 	make crosstool
 
 $(TARGET_DIR)/lib/libc.so.6:
-	$(SILENT)if test -e $(CROSS_BASE)/$(TARGET)/sys-root/lib; then \
+	if test -e $(CROSS_BASE)/$(TARGET)/sys-root/lib; then \
 		cp -a $(CROSS_BASE)/$(TARGET)/sys-root/lib/*so* $(TARGET_DIR)/lib; \
 	else \
 		cp -a $(CROSS_BASE)/$(TARGET)/lib/*so* $(TARGET_DIR)/lib; \
@@ -15,9 +15,10 @@ $(TARGET_DIR)/lib/libc.so.6:
 #
 # crosstool-ng
 #
-CROSSTOOL_NG_VER = 1dbb06f2
-CROSSTOOL_NG_SOURCE = crosstool-ng-$(CROSSTOOL_NG_VER).tar.bz2
+CROSSTOOL_NG_VER = 872341e3
+CROSSTOOL_NG_SOURCE = crosstool-ng-git-$(CROSSTOOL_NG_VER).tar.bz2
 CROSSTOOL_NG_URL = https://github.com/crosstool-ng/crosstool-ng.git
+
 ifeq ($(BOXTYPE), vusolo4k)
 CROSSTOOL_BOXTYPE_PATCH = $(PATCHES)/ct-ng/crosstool-ng-$(CROSSTOOL_NG_VER)-vusolo4k.patch
 endif
@@ -37,15 +38,15 @@ ifeq ($(wildcard $(CROSS_BASE)/build.log.bz2),)
 CROSSTOOL = crosstool
 crosstool: crosstool-ng
 
-crosstool-ng: directories $(ARCHIVE)/$(KERNEL_SRC) $(ARCHIVE)/$(CROSSTOOL_NG_SOURCE)
+crosstool-ng: $(D)/directories $(ARCHIVE)/$(KERNEL_SRC) $(ARCHIVE)/$(CROSSTOOL_NG_SOURCE)
 	make $(BUILD_TMP)
-	$(SILENT)if [ ! -e $(CROSS_BASE) ]; then \
+	if [ ! -e $(CROSS_BASE) ]; then \
 		mkdir -p $(CROSS_BASE); \
 	fi;
 	$(REMOVE)/crosstool-ng-$(CROSSTOOL_NG_VER)
 	$(UNTAR)/$(CROSSTOOL_NG_SOURCE)
-	$(SILENT)unset CONFIG_SITE LIBRARY_PATH CPATH C_INCLUDE_PATH PKG_CONFIG_PATH CPLUS_INCLUDE_PATH INCLUDE; \
-	set -e; cd $(BUILD_TMP)/crosstool-ng-$(CROSSTOOL_NG_VER); \
+	unset CONFIG_SITE LIBRARY_PATH CPATH C_INCLUDE_PATH PKG_CONFIG_PATH CPLUS_INCLUDE_PATH INCLUDE; \
+	set -e; cd $(BUILD_TMP)/crosstool-ng-git-$(CROSSTOOL_NG_VER); \
 		cp -a $(PATCHES)/ct-ng/crosstool-ng-$(CROSSTOOL_NG_VER)-$(BOXARCH)-$(BOXTYPE).config .config; \
 		NUM_CPUS=$$(expr `getconf _NPROCESSORS_ONLN` \* 2); \
 		MEM_512M=$$(awk '/MemTotal/ {M=int($$2/1024/512); print M==0?1:M}' /proc/meminfo); \
@@ -54,7 +55,6 @@ crosstool-ng: directories $(ARCHIVE)/$(KERNEL_SRC) $(ARCHIVE)/$(CROSSTOOL_NG_SOU
 		sed -i "s@^CT_PARALLEL_JOBS=.*@CT_PARALLEL_JOBS=$$NUM_CPUS@" .config; \
 		\
 		$(call apply_patches,$(CROSSTOOL_BOXTYPE_PATCH)); \
-		cp $(PATCHES)/ct-ng/gcc-6.3-backport-fix-of-check-for-empty-string-in-ubsan.c.patch patches/gcc/linaro-6.3-2017.02; \
 		\
 		export CT_NG_ARCHIVE=$(ARCHIVE); \
 		export CT_NG_BASE_DIR=$(CROSS_BASE); \
@@ -62,28 +62,24 @@ crosstool-ng: directories $(ARCHIVE)/$(KERNEL_SRC) $(ARCHIVE)/$(CROSSTOOL_NG_SOU
 		export CT_NG_CUSTOM_KERNEL_VER=$(CUSTOM_KERNEL_VER); \
 		export LD_LIBRARY_PATH=; \
 		test -f ./configure || ./bootstrap && \
-		./configure $(SILENT_CONFIGURE) --enable-local; \
+		./configure --enable-local; \
 		MAKELEVEL=0 make; \
 		chmod 0755 ct-ng; \
 		./ct-ng oldconfig; \
 		./ct-ng build
-	$(SILENT)chmod -R +w $(CROSS_BASE)
-	$(SILENT)test -e $(CROSS_BASE)/$(TARGET)/lib || ln -sf sys-root/lib $(CROSS_BASE)/$(TARGET)/
-	$(SILENT)rm -f $(CROSS_BASE)/$(TARGET)/sys-root/lib/libstdc++.so.6.0.20-gdb.py
-	$(REMOVE)/crosstool-ng-$(CROSSTOOL_NG_VER)
-	@echo "--------------------------------------------------------------"
-	@echo -e "Build of $(TERM_GREEN_BOLD)$(PKG_NAME)$(TERM_NORMAL) completed."
-	@echo
+	chmod -R +w $(CROSS_BASE)
+	test -e $(CROSS_BASE)/$(TARGET)/lib || ln -sf sys-root/lib $(CROSS_BASE)/$(TARGET)/
+	rm -f $(CROSS_BASE)/$(TARGET)/sys-root/lib/libstdc++.so.6.0.20-gdb.py
+	$(REMOVE)/crosstool-ng-git-$(CROSSTOOL_NG_VER)
 endif
 
-crossmenuconfig: directories $(ARCHIVE)/$(CROSSTOOL_NG_SOURCE)
-	$(REMOVE)/crosstool-ng-$(CROSSTOOL_NG_VER)
+crossmenuconfig: $(D)/directories $(ARCHIVE)/$(CROSSTOOL_NG_SOURCE)
+	$(REMOVE)/crosstool-ng-git-$(CROSSTOOL_NG_VER)
 	$(UNTAR)/$(CROSSTOOL_NG_SOURCE)
-	$(SET) -e; unset CONFIG_SITE; cd $(BUILD_TMP)/crosstool-ng-$(CROSSTOOL_NG_VER); \
+	set -e; unset CONFIG_SITE; cd $(BUILD_TMP)/crosstool-ng-git-$(CROSSTOOL_NG_VER); \
 		cp -a $(PATCHES)/ct-ng/crosstool-ng-$(CROSSTOOL_NG_VER)-$(BOXARCH)-$(BOXTYPE).config .config; \
 		test -f ./configure || ./bootstrap && \
-		./configure $(SILENT_CONFIGURE) --enable-local; \
+		./configure --enable-local; \
 		MAKELEVEL=0 make; \
 		chmod 0755 ct-ng; \
 		./ct-ng menuconfig
-
