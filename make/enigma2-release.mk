2bf8a838dbe33885b663f8eaef1a19ee99985447
Audioniek <audioniek@xs4all.nl>
2019-05-13 18:34:37 +0200

    adb_box: add kernel patch level P0217

--- make/enigma2-release.mk
+++ make/enigma2-release.mk
@@ -668,7 +668,7 @@ endif
 #
 #
 	$(SILENT)cp $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/extra/simu_button/simu_button.ko $(RELEASE_DIR)/lib/modules/
-ifneq ($(BOXTYPE), $(filter $(BOXTYPE), vip2_v1 spark spark7162))
+ifneq ($(BOXTYPE), $(filter $(BOXTYPE), vip2_v1 spark spark7162 adb_box))
 	$(SILENT)cp $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/extra/cic/*.ko $(RELEASE_DIR)/lib/modules/
 endif
 	$(SILENT)[ -e $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/extra/button/button.ko ] && cp $(TARGET_DIR)/lib/modules/$(KERNEL_VER)/extra/button/button.ko $(RELEASE_DIR)/lib/modules/ || true
