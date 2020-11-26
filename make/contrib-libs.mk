dc2e83dcf8859c6a6e144de11499477bdcb140db
Audioniek <audioniek@xs4all.nl>
2020-11-26 02:28:39 +0100

    alsa-utils/lib-> 1.2.4

--- make/contrib-libs.mk
+++ make/contrib-libs.mk
@@ -2038,7 +2038,7 @@ $(D)/libusb_compat: $(D)/bootstrap $(D)/libusb $(ARCHIVE)/$(LIBUSB_COMPAT_SOURCE
 #
 # alsa-lib
 #
-ALSA_LIB_VER = 1.2.3.2
+ALSA_LIB_VER = 1.2.4
 ALSA_LIB_SOURCE = alsa-lib-$(ALSA_LIB_VER).tar.bz2
 ALSA_LIB_PATCH  = alsa-lib-$(ALSA_LIB_VER).patch
 ALSA_LIB_PATCH += alsa-lib-$(ALSA_LIB_VER)-link_fix.patch
@@ -2082,7 +2082,7 @@ $(D)/alsa_lib: $(D)/bootstrap $(ARCHIVE)/$(ALSA_LIB_SOURCE)
 #
 # alsa-utils
 #
-ALSA_UTILS_VER = 1.2.3
+ALSA_UTILS_VER = 1.2.4
 ALSA_UTILS_SOURCE = alsa-utils-$(ALSA_UTILS_VER).tar.bz2
 ALSA_UTILS_PATCH  = alsa-utils-$(ALSA_UTILS_VER).patch
 
