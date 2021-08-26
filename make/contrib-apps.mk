e2a7a9d6cf9f11055b7ce563420066e2a3f9ed5b
Audioniek <audioniek@xs4all.nl>
2021-08-26 13:13:06 +0200

    ethtool -> 5.13

--- make/contrib-apps.mk
+++ make/contrib-apps.mk
@@ -1361,7 +1361,7 @@ $(D)/htop: $(D)/bootstrap $(D)/ncurses $(ARCHIVE)/$(HTOP_SOURCE)
 #
 # ethtool
 #
-ETHTOOL_VER = 5.12
+ETHTOOL_VER = 5.13
 ETHTOOL_SOURCE = ethtool-$(ETHTOOL_VER).tar.xz
 ETHTOOL_PATCH = ethtool-$(ETHTOOL_VER).patch
 
