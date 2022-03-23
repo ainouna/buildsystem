b98dfca95f5803fde2df1783b3f29612885860c2
Audioniek <audioniek@xs4all.nl>
2022-03-23 19:22:38 +0100

    openvpn -> 2.5.5

--- make/contrib-apps.mk
+++ make/contrib-apps.mk
@@ -1655,7 +1655,7 @@ $(D)/udpxy: $(D)/bootstrap $(ARCHIVE)/$(UDPXY_SOURCE)
 #
 # openvpn
 #
-OPENVPN_VER = 2.5.4
+OPENVPN_VER = 2.5.5
 OPENVPN_SOURCE = openvpn-$(OPENVPN_VER).tar.xz
 
 $(ARCHIVE)/$(OPENVPN_SOURCE):
