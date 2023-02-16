7ea38452231763e35b2ce396e9f8028b717ccf64
Audioniek <audioniek@xs4all.nl>
2023-02-17 00:53:36 +0100

    licurl -> 7.86.0

--- make/contrib-libs.mk
+++ make/contrib-libs.mk
@@ -1024,7 +1024,7 @@ $(D)/libconfig: $(D)/bootstrap $(ARCHIVE)/$(LIBCONFIG_SOURCE)
 #
 # libcurl
 #
-LIBCURL_VER = 7.85.0
+LIBCURL_VER = 7.86.0
 LIBCURL_SOURCE = curl-$(LIBCURL_VER).tar.bz2
 LIBCURL_PATCH = libcurl-$(LIBCURL_VER).patch
 
