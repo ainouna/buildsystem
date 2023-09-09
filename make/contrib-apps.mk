ba748aa9a4fb4d06d1575902b439fdf66d4f6bdc
Audioniek <audioniek@xs4all.nl>
2023-09-09 15:17:53 +0200

    fix midnight commander ftp file listing (thx Andrew Borodin)

--- make/contrib-apps.mk
+++ make/contrib-apps.mk
@@ -659,6 +659,7 @@ $(D)/ntfs_3g: $(D)/bootstrap $(ARCHIVE)/$(NTFS_3G_SOURCE)
 MC_VER = 4.8.29
 MC_SOURCE = mc-$(MC_VER).tar.xz
 MC_PATCH  = mc-$(MC_VER).patch
+MC_PATCH += mc-$(MC_VER)-fix_ftp_listing.patch
 
 $(ARCHIVE)/$(MC_SOURCE):
 	$(WGET) ftp.midnight-commander.org/$(MC_SOURCE)
